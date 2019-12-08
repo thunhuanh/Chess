4f76aac4ed3258411469a1e9e5737c0265edb27a
Mizukume <Maemikon99@gmail.com>
2019-12-08 09:31:38 +0700

    Ok

--- client/src/components/pages/HomePage/HomePage.js
+++ client/src/components/pages/HomePage/HomePage.js
@@ -10,25 +10,15 @@ import OnlinePlayer from "./OnlinePlayer";
 export default function HomePage (){
     return (
     <div className='homePage'>
-       <div className='homePageNav'>
+        <div className='homePageNav'>
            <img src="./Style/IMG/Chess.png" alt="Can not display"></img>
            <p>NAVBAR</p>
-       </div>
-        <div className='homePageComponents'>
-            <div className='homePageMainComponents'>
-                <div className='playModeBorderBox'>
-                    <PlayMode/>
-                </div>
-                <HomeChatBox/>
-            </div>
-            <div className="homePageAuxComponents">
-
-                <div className='friendListBorderBox'>
-                    <FriendList/>
-                </div>
-                <OnlinePlayer/>
-            </div>
         </div>
-    </div>
+            <PlayMode></PlayMode>
+                
+            <LeftSide></LeftSide>    
+            <ChatBox></ChatBox>
+            <RightSide></RightSide>
+        </div>
     )
 }
