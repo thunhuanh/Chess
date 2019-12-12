1d3cd20fd45ff616e76138b9b449225cd56413f8
tien nguyen <nvuongtien@gmail.com>
2019-12-12 16:29:51 +0700

    fix moveHistory

--- client/src/components/pages/chessPage/MoveHistory.js
+++ client/src/components/pages/chessPage/MoveHistory.js
@@ -1,84 +1,41 @@
 import React from 'react'
+import ReactTable from 'react-table-v6'
+import 'react-table-v6/react-table.css'
 import './styles/MoveHistory.css'
+
+const columns = [{
+  Header: 'Turn',
+  accessor: 'turn'
+}, {
+  Header: 'Move',
+  accessor: 'move',
+}
+]
 
 class MoveHistory extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+           data:[
+            {
+              name: 'tien',
+              age: '12'
+             },
+           ]
         }
     }
+    
     render() {
         return(
             <div className='moveHistory'>
                 <p id='mhTitle'>Move history</p>
                 <div className='moveList'>
-                    <table id='moveTable'>
-                        <thead>
-                            <tr>
-                                <th >Turn</th>
-                                <th >Move</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th></th>
-                                <th></th>
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                <th></th>
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                <th></th>
-                              
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                <th></th>
-                                
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                 <th></th>
-                             
-                            </tr>
-
-                            <tr>
-                              <th></th>
-                              <th></th>
-                               
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                <th></th>
-                           
-                            </tr>
-
-                            <tr>
-                             <th></th>
-                             <th></th>
-                             
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                <th></th>
-                            </tr>
-
-                            <tr>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </tbody>
-                    </table>
+                <ReactTable
+                  data={this.state.data}
+                  columns={columns}
+                  showPagination={false}
+                />
                 </div>
             </div>
         );
